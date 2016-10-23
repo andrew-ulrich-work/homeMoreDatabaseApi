560e90bef4311972b49bf3a47efdafd9f7c015ec
andrew_ulrich <andrewfulrich@gmail.com>
2016-10-22 19:33:50 -0500

    added name and title to atRisk

--- api/controllers/showAtRisk.js
+++ api/controllers/showAtRisk.js
@@ -1,8 +1,19 @@
 'use strict';
 var faker = require('json-schema-faker');
+// faker.format('name', function(gen, schema) {
+  // return gen.faker.name.firstName();
+// });
+// faker.format('title', function(gen, schema) {
+  // return gen.faker.hacker.phrase();
+// });
+
 var atRiskResponse = {
 	"type": "object",
 	"properties": {
+    "fakeName": {
+			"type": "string",
+			"enum":["Anne","Marie","Claire","Stella","Georgia","Virginia","Isabelle","Florence","Jane","Wanda","Ellen","Jenna","Rebecca","Sarah","Elizabeth","Bethany"]
+		},
 		"description": {
 			"type": "string",
 			"maxLength": 40
@@ -11,7 +22,7 @@ var atRiskResponse = {
 			"type": "array",
 			"items": {
 				"type": "string",
-				"maxLength": 20
+				"enum":["Anne","Marie","Claire","Stella","Georgia","Virginia","Isabelle","Florence","Jane","Wanda","Ellen","Jenna","Rebecca","Sarah","Elizabeth","Bethany"]
 			}
 		},
 		"stories": {
@@ -19,6 +30,10 @@ var atRiskResponse = {
 			"items": {
 				"type": "object",
 				"properties": {
+          "title": {
+						"type": "string",
+            "enum":["I need to pay my utility bill","my car got reposessed!","I can't get to work","I am near my wits' end", "Please help me","Just need a little help","My car broke down","Someone broke into our house","I was robbed","I am getting help","I can't seem to get back on track","Too many bills to keep up","Truly drowning in debt"]
+					},
 					"text": {
 						"type": "string"
 					},
@@ -36,11 +51,11 @@ var atRiskResponse = {
 						"minimum": 0
 					}
 				},
-        "required":["text","amountRaised","amountNeeded","dueDate","contributors"]
+        "required":["title","text","amountRaised","amountNeeded","dueDate","contributors"]
 			}
 		}
 	},
-  "required":["description","followers","stories"]
+  "required":["name","description","followers","stories"]
 }
 module.exports = {
   showAllAtRisk: showAllAtRisk,
@@ -49,13 +64,18 @@ module.exports = {
 
 function show(req,res) {
 var fakeData=faker(atRiskResponse);
+//fakeData.fakeName=faker.name.firstName();
+//fakeData.stories.forEach((story)=>{ story.title=faker.hacker.phrase(); });
   res.json(fakeData);
 }
 
 function showAllAtRisk(req,res) {
 var fakeData=[];
 for(var i =0; i< 10; i++) {
-  fakeData.push(faker(atRiskResponse));
+  var fakeRecord=faker(atRiskResponse)
+  //fakeRecord.fakeName=faker.name.firstName();
+  //fakeRecord.stories.forEach((story)=>{ story.title=faker.hacker.phrase(); });
+  fakeData.push(fakeRecord);
 }
   res.json(fakeData);
 }
